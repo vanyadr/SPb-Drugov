1f73906dceac6ddcd231865f4a66b3ef7e3514a7
vanyadr <vanyadr@yandex.ru>
2021-11-18 18:11:14 +0300

    init2

--- main.py
+++ main.py
@@ -5,11 +5,44 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPaintEvent, QPainter, QColor
 from PyQt5.QtWidgets import QColorDialog, QApplication, QMainWindow
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Window(QMainWindow):
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(210, 190, 381, 351))
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(370, 100, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "DRAW"))
+
+
+class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("Ui.ui", self)
+        self.setupUi(self)
         self.painting = False
         self.pushButton.clicked.connect(self.startPaint)
 
@@ -26,7 +59,10 @@ class Window(QMainWindow):
             self.painting = False
 
     def draw(self, qp: QPainter):
-        qp.setBrush(QColor(255, 255, 0))
+        a = random.randint(0, 255)
+        b = random.randint(0, 255)
+        c = random.randint(0, 255)
+        qp.setBrush(QColor(a, b, c))
         a = random.randint(1, 300)
         qp.drawEllipse(300, 200, a, a)
 
